6a7afedb0c2d5377685cff2cf6a1d99fcd1148d2
RenoirTan <renoirtan2005@gmail.com>
2021-06-09 15:56:02 +0800

    v0.2.0: Scripts documentation

--- scripts/style.py
+++ scripts/style.py
@@ -3,6 +3,24 @@ from typing import *
 from . import utils
 
 class Style(object):
+    """
+    An object representing the color, opacity and font style of an element
+    or text in VSCode.
+
+    This object can be generated from a string in the follow format:
+
+        {color}/{opacity}/{font style}
+    
+    where
+    - color can be a valid hex string or name of a color,
+    - opacity is the hexadecimal value of the alpha channel
+    (between 0 and 0xff)
+    - style can be /[ibu]*/.
+        - i: Italic
+        - b: Bold
+        - u: Underline
+    """
+
     def __init__(self, literal: str) -> None:
         components = literal.split("/")
         self.color: Optional[str] = utils.option_string(
@@ -14,9 +32,14 @@ class Style(object):
             raise ValueError(
                 f"opacity must be between 0 and 255: {self.opacity}"
             )
-        self.style: Optional[str] = utils.option_string(utils.list_get(components, 2))
+        self.style: Optional[str] = utils.option_string(
+            utils.list_get(components, 2)
+        )
 
     def inherit(self, parent: Style) -> Style:
+        """
+        Fill in the blanks using the style from a parent.
+        """
         if self.color is None:
             self.color = parent.color
         if self.opacity is None:
@@ -27,6 +50,9 @@ class Style(object):
     def get_color(
         self, palette: Dict[str, str], generator_colors: Dict[str, str]
     ) -> Optional[str]:
+        """
+        Get the hex color value of this style.
+        """
         if self.color is None:
             return None
         color = self.color
@@ -42,6 +68,9 @@ class Style(object):
         return color
     
     def get_style(self) -> Optional[str]:
+        """
+        Get the font style.
+        """
         if self.style is None:
             return None
         style = []
--- scripts/utils.py
+++ scripts/utils.py
@@ -3,21 +3,31 @@ from typing import *
 
 
 STRING_EMPTY = re.compile("^\s*$")
-HEX6 = re.compile("^#[0-9a-f]{6}$")
-HEX8 = re.compile("^#[0-9a-f]{8}$")
-HEX3 = re.compile("^#[0-9a-f]{3}$")
-HEX4 = re.compile("^#[0-9a-f]{4}$")
+HEX6 = re.compile("^#[0-9a-f]{6}$", flags=re.IGNORECASE)
+HEX8 = re.compile("^#[0-9a-f]{8}$", flags=re.IGNORECASE)
+HEX3 = re.compile("^#[0-9a-f]{3}$", flags=re.IGNORECASE)
+HEX4 = re.compile("^#[0-9a-f]{4}$", flags=re.IGNORECASE)
 
 
 def is_string_blank(string: str) -> bool:
+    """
+    Check to see if a string is empty or only has whitespace.
+    """
     return STRING_EMPTY.match(string) is not None
 
 
 def option_string(string: str) -> Optional[str]:
+    """
+    Convert an string to None if it's blank (i.e. only whitespace),
+    otherwise return the original string.
+    """
     return None if is_string_blank(string) else string
 
 
 def check_hex_color(string: str) -> int:
+    """
+    Check whether a string represents a valid hex colour.
+    """
     if HEX6.match(string):
         return 1
     elif HEX8.match(string):
@@ -31,6 +41,10 @@ def check_hex_color(string: str) -> int:
 
 
 def list_get(array: List[Any], index: int, default: Any = None) -> Any:
+    """
+    Get an element of an array at a specified index. If the index is out
+    of bounds, return a given default value.
+    """
     try:
         return array[index]
     except IndexError as e:
